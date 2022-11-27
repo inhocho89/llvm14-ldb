71e277b39e4939589cad14e50e3365ca92db1ea8
Inho Cho <inhocho@csail.mit.edu>
2022-11-27 12:38:16 -0700

    New tag API: allow multiple tags at the same time

--- libldb/include/ldb/tag.h
+++ libldb/include/ldb/tag.h
@@ -1,5 +1,6 @@
 #pragma once
 
 void ldb_tag_set(uint64_t tag);
+void ldb_tag_unset(uint64_t tag);
 void ldb_tag_clear();
 void ldb_tag_block(uint64_t tag);
--- libldb/tag.c
+++ libldb/tag.c
@@ -5,8 +5,12 @@ void ldb_tag_set(uint64_t tag) {
   event_record_now(LDB_EVENT_TAG_SET, tag, 0, 0);
 }
 
+void ldb_tag_unset(uint64_t tag) {
+  event_record_now(LDB_EVENT_TAG_UNSET, tag, 0, 0);
+}
+
 void ldb_tag_clear() {
-  ldb_tag_set(0);
+  event_record_now(LDB_EVENT_TAG_CLEAR, 0, 0, 0);
 }
 
 void ldb_tag_block(uint64_t tag) {
