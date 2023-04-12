204b4a6614b812ac52d599d68a525f17b83de1b1
Inho Cho <inhocho@csail.mit.edu>
2023-04-12 07:18:19 -0400

    New monitoring thread implementation to reduce probling effect (by Adam)

--- libldb/monitor.c
+++ libldb/monitor.c
@@ -2,226 +2,207 @@
 #include <time.h>
 #include "common.h"
 
-#define LDB_MONITOR_PERIOD 1
+#define LDB_MONITOR_PERIOD 0
 
 extern ldb_shmseg *ldb_shared;
 extern bool running;
 
-static inline bool is_stack_modified(char ***fsbase, char *slock,
-    uint64_t slock2) {
-  return (*fsbase == NULL || slock != *(*fsbase - 35) ||
-        slock2 != *(uint64_t *)(*fsbase - 43));
+#define ACCESS_ONCE(x) (*(volatile typeof(x) *)&(x))
+
+static void clock_get_now(struct timespec *ts) {
+  barrier();
+  clock_gettime(CLOCK_MONOTONIC_RAW, ts);
+  barrier();
+}
+
+static uint64_t elapsed_ns(struct timespec *start, struct timespec *end) {
+  return (end->tv_sec - start->tv_sec) * 1000000000 +
+	 (end->tv_nsec - start->tv_nsec);
+}
+
+struct tls_shared_region {
+  uint64_t gen;
+  char pad[56];
+  uint64_t rbp;
+};
+
+struct stack_sample {
+  uint64_t rip;
+  uint64_t rbp;
+  uint64_t gen;
+  struct timespec start_ts;
+};
+
+struct thread {
+  ldb_event_buffer_t *ebuf;
+  pid_t thread_id;
+  bool last_ts_set;
+  int cnt;
+  uint64_t last_gen;
+  uint64_t *stack_base;
+  struct timespec last_ts; 
+  struct tls_shared_region *tls;
+  struct stack_sample samples[LDB_MAX_CALLDEPTH];
+};
+
+static int scan_stack(struct thread *th, uint64_t *rbp, struct stack_sample *sarr) {
+  uint64_t *top = rbp - 1;
+  int frames = 0;
+
+  // Heuristic: If start rbp is not valid, skip this iteration
+  if (rbp <= (uint64_t *)0x7f0000000000 || rbp > th->stack_base)
+    return -1;
+
+  while (rbp) {
+    // check for invalid base pointers
+    if (rbp <= top || rbp > th->stack_base) {
+      return -1; 
+    }
+
+    uint64_t canary_and_tag = *(rbp + 1);
+    uint32_t canary = (uint32_t)(canary_and_tag >> 32);
+    if (canary != LDB_CANARY) {
+      return -1;
+    }
+    sarr->gen = *(rbp + 2);
+    sarr->rip = *(rbp + 3);
+    sarr->rbp = (uint64_t)rbp;
+    sarr++;
+    frames++;
+
+    if (frames > LDB_MAX_CALLDEPTH) {
+      printf("[WARNING] max call depth exceeded\n");
+      return -1;
+    }
+
+    rbp = (uint64_t *)*rbp;
+  }
+
+  return frames;
+}
+
+static void
+emit_stack_samples(struct thread *th, struct stack_sample *samples,
+		   int pos, struct timespec now) {
+  uint64_t latency;
+  int i;
+
+  for (i = 0; i < th->cnt; i++) {
+    struct stack_sample *s = &th->samples[i];
+    if (pos >= 1 && s->gen == samples[--pos].gen) {
+      if (pos >= 0 && s->rbp != samples[pos].rbp) {
+        printf("oops rbp %lx %lx rip %lx %lx\n", s->rbp, samples[pos].rbp, s->rip, samples[pos].rip);
+      }
+      
+      continue;
+    }
+
+    latency = elapsed_ns(&s->start_ts, &now);
+    event_record(th->ebuf, LDB_EVENT_STACK, now, th->thread_id,
+                 latency, s->rip, s->gen);
+    //printf("gen %ld id %d rip %lx latency %ld\n", s->gen, th->thread_id, s->rip, latency);
+  }
+}
+
+static void scan_thread(struct thread *th) {
+  struct stack_sample tmp[LDB_MAX_CALLDEPTH];
+  struct timespec now;
+  int frames, i;
+
+  // Skip if a modification wasn't detected (to avoid probe effects)
+  uint64_t gen = ACCESS_ONCE(th->tls->gen);
+  uint64_t rbp = ACCESS_ONCE(th->tls->rbp);
+  if (gen == th->last_gen) {
+    return;
+  }
+
+  // Get the current time
+  clock_get_now(&now);
+
+  // Scan the stack and gather frames 
+  frames = scan_stack(th, (uint64_t *)rbp, tmp);
+  uint64_t end_gen = ACCESS_ONCE(th->tls->gen);
+  uint64_t end_rbp = ACCESS_ONCE(th->tls->rbp);
+  if (frames < 0 || end_gen != gen || end_rbp != rbp) {
+    if (!th->last_ts_set) {
+      th->last_ts_set = true;
+      th->last_ts = now;
+    }
+    return;
+  }
+  th->last_gen = gen;
+
+  // Emit stack samples
+  if (!th->last_ts_set)
+    th->last_ts = now;
+  emit_stack_samples(th, tmp, frames, th->last_ts);
+  th->last_ts_set = false;
+
+  // Store frames (in reverse so top is first) for next comparison
+  for (i = 0; i < frames; i++) {
+    //printf("i %d rbp %lx rip %lx\n", i, th->samples[i].rbp, th->samples[i].rip); 
+    //printf("i %d rbp %lx rip %lx\n", i, tmp[frames - i - 1].rbp, tmp[frames - i - 1].rip);
+    th->samples[i].rip = tmp[frames - i - 1].rip;
+    th->samples[i].rbp = tmp[frames - i - 1].rbp;
+
+    // don't update the time if the frame hasn't changed
+    if (tmp[frames - i - 1].gen == th->samples[i].gen)
+      continue;
+    th->samples[i].gen = tmp[frames - i - 1].gen;
+    th->samples[i].start_ts = now;
+  }
+  th->cnt = frames;
 }
 
 void *monitor_main(void *arg) {
-  // stats to collect
-  uint32_t **ldb_tag;
-  uint64_t **ldb_ngen;
-  char ***ldb_rip;
-  char ***ldb_rbp;
-  uint64_t **ldb_latency;
-  int *ldb_cnt;
-
-  // temporary variables
-  uint32_t temp_tag[LDB_MAX_CALLDEPTH];
-  uint64_t temp_ngen[LDB_MAX_CALLDEPTH];
-  char *temp_rip[LDB_MAX_CALLDEPTH];
-  char *temp_rbp[LDB_MAX_CALLDEPTH];
-
+  int i;
+#if LDB_MONITOR_PERIOD > 0
   struct timespec scan_start;
   struct timespec scan_finish;
   uint64_t scan_delay;
+#endif
 
+  // Pin to core 0
   cpu_set_t cpuset;
-
   CPU_ZERO(&cpuset);
   CPU_SET(0, &cpuset);
   pthread_setaffinity_np(pthread_self(), sizeof(cpuset), &cpuset);
 
-  // allocate memory for bookkeeping
-  ldb_tag = (uint32_t **)malloc(LDB_MAX_NTHREAD * sizeof(uint32_t *));
-  ldb_ngen = (uint64_t **)malloc(LDB_MAX_NTHREAD * sizeof(uint64_t *));
-  ldb_rip = (char ***)malloc(LDB_MAX_NTHREAD * sizeof(char **));
-  ldb_rbp = (char ***)malloc(LDB_MAX_NTHREAD * sizeof(char **));
-  ldb_latency = (uint64_t **)malloc(LDB_MAX_NTHREAD * sizeof(uint64_t *));
-  ldb_cnt = (int *)malloc(LDB_MAX_NTHREAD * sizeof(int));
-
-  for (int i = 0; i < LDB_MAX_NTHREAD; ++i) {
-    ldb_tag[i] = (uint32_t *)malloc(LDB_MAX_CALLDEPTH * sizeof(uint32_t));
-    ldb_ngen[i] = (uint64_t *)malloc(LDB_MAX_CALLDEPTH * sizeof(uint64_t));
-    ldb_rip[i] = (char **)malloc(LDB_MAX_CALLDEPTH * sizeof(char *));
-    ldb_rbp[i] = (char **)malloc(LDB_MAX_CALLDEPTH * sizeof(char *));
-    ldb_latency[i] = (uint64_t *)malloc(LDB_MAX_CALLDEPTH * sizeof(uint64_t));
+  // allocate per-thread memory
+  struct thread *threads = calloc(sizeof(*threads), LDB_MAX_NTHREAD);
+  ldb_event_buffer_t *ebuf = ldb_shared->ldb_thread_infos[get_thread_info_idx()].ebuf;
+  for (i = 0; i < LDB_MAX_NTHREAD; i++) {
+    struct thread *th = &threads[i];
+    th->ebuf = ebuf;
   }
 
-  memset(ldb_cnt, 0, sizeof(int) * LDB_MAX_NTHREAD);
-
-  ldb_event_buffer_t *ebuf = ldb_shared->ldb_thread_infos[get_thread_info_idx()].ebuf;
-
-  printf("Monitor thread starts\n");
-  
-  // Currently busy-running
+  printf("Monitor v2 thread starts. monitoring period = %d us\n", LDB_MONITOR_PERIOD);
   while (running) {
 #if LDB_MONITOR_PERIOD > 0
-    barrier();
-    clock_gettime(CLOCK_MONOTONIC_RAW, &scan_start);
-    barrier();
+    clock_get_now(&scan_start);
 #endif
-    for (int tidx = 0; tidx < ldb_shared->ldb_max_idx; ++tidx) {
-      // Skip if fsbase is invalid
-      if (ldb_shared->ldb_thread_infos[tidx].fsbase == NULL) {
+    for (i = 0; i < LDB_MAX_NTHREAD; i++) {
+      struct thread *th = &threads[i];
+      if (ldb_shared->ldb_thread_infos[i].id == 0)
         continue;
-      }
 
-      pid_t thread_id = ldb_shared->ldb_thread_infos[tidx].id;
-      char ***fsbase = &(ldb_shared->ldb_thread_infos[tidx].fsbase);
-      char *stack_base = ldb_shared->ldb_thread_infos[tidx].stackbase;
-      struct timespec *last_ts = &ldb_shared->ldb_thread_infos[tidx].ts_scan;
-      struct timespec now;
-      uint64_t elapsed;
-      int lidx = 0;
-      char *rbp = *(*fsbase - 35);
-      uint64_t slock2 = *(uint64_t *)(*fsbase - 43);
-      char *prbp = rbp - 8;
-      // use initial rbp as sequence lock
-      char *slock = rbp;
-      uint64_t ngen = 99;
-      uint64_t canary_and_tag;
-      uint32_t tag;
-      uint32_t canary;
-      char *rip;
+      // initialize per-thread state
+      th->thread_id = ldb_shared->ldb_thread_infos[i].id;
+      uint64_t *tmp = (uint64_t *)ldb_shared->ldb_thread_infos[i].fsbase;
+      th->tls = (struct tls_shared_region *)(tmp - 43);
+      th->stack_base = (uint64_t *)ldb_shared->ldb_thread_infos[i].stackbase;
 
-      barrier();
-      clock_gettime(CLOCK_MONOTONIC_RAW, &now);
-      barrier();
-      elapsed = (now.tv_sec - last_ts->tv_sec) * 1000000000 + (now.tv_nsec - last_ts->tv_nsec);
-
-      // Heuristic check if rbp is not valid, skip this iteration
-      if (rbp <= (char *)0x7f0000000000 || rbp > stack_base) {
-        continue;
-      }
-
-      // traversing stack frames
-      while (rbp != NULL) {
-        // check whether RBP is valid
-        if (rbp <= prbp || rbp > stack_base) {
-          break;
-        }
-
-        // check whether stack has been modified and fetch data
-        barrier();
-        if (is_stack_modified(fsbase, slock, slock2)) {
-          lidx = 0;
-          break;
-        }
-
-        canary_and_tag = *((uint64_t *)(rbp + 8));
-        canary = (uint32_t)(canary_and_tag >> 32);
-        tag = (uint32_t)(canary_and_tag & 0xffffffff);
-        ngen = *((uint64_t *)(rbp + 16));
-        rip = (char *)(*((uint64_t *)(rbp + 24)));
-
-        // check if stack frame is valid
-        if (canary != LDB_CANARY) {
-          break;
-        }
-
-        // all check passed!
-        // store frame info to locals
-        temp_tag[lidx] = tag;
-        temp_ngen[lidx] = ngen;
-        temp_rip[lidx] = rip;
-        temp_rbp[lidx] = rbp;
-
-        // go up for next frame
-        barrier();
-        if(is_stack_modified(fsbase, slock, slock2)) {
-          lidx = 0;
-          break;
-        }
-
-        prbp = rbp;
-        rbp = (char *)(*((uint64_t *)rbp));
-        lidx++;
-      }
-
-      // No data collected
-      if (lidx == 0) {
-        continue;
-      }
-
-      // Update latency
-      int gidx = 0;
-
-      while (gidx < ldb_cnt[tidx] && ldb_rbp[tidx][gidx] > temp_rbp[lidx-1]) {
-        ldb_latency[tidx][gidx] += elapsed;
-        gidx++;
-      }
-
-      while (gidx < ldb_cnt[tidx] && lidx > 0) {
-        if (ldb_ngen[tidx][gidx] != temp_ngen[lidx-1])
-          break;
-        ldb_latency[tidx][gidx] += elapsed;
-        gidx++;
-        lidx--;
-      }
-
-      if (gidx > LDB_MAX_CALLDEPTH) {
-        printf("[WARNING] gidx overflow: %d\n", gidx);
-      }
-
-      // Record finished function call
-      for (int i = gidx; i < ldb_cnt[tidx]; ++i) {
-        event_record(ebuf, LDB_EVENT_STACK, now, thread_id, ldb_latency[tidx][i],
-            (uintptr_t)ldb_rip[tidx][i], ldb_ngen[tidx][i]);
-      }
-
-      // Add new nodes
-      while (lidx > 0) {
-        ldb_tag[tidx][gidx] = temp_tag[lidx - 1];
-        ldb_ngen[tidx][gidx] = temp_ngen[lidx - 1];
-        ldb_latency[tidx][gidx] = 0;
-        ldb_rip[tidx][gidx] = temp_rip[lidx - 1];
-        ldb_rbp[tidx][gidx] = temp_rbp[lidx - 1];
-
-        gidx++;
-        lidx--;
-      }
-
-      if (gidx > LDB_MAX_CALLDEPTH) {
-        printf("[WARNING] gidx overflow: %d\n", gidx);
-      }
-
-      ldb_cnt[tidx] = gidx;
-      *last_ts = now;
-    } // for
+      // scan the thread's stack
+      scan_thread(th);
+    }
 #if LDB_MONITOR_PERIOD > 0
-    barrier();
-    clock_gettime(CLOCK_MONOTONIC_RAW, &scan_finish);
-    barrier();
-    scan_delay = (scan_finish.tv_sec - scan_start.tv_sec) * 1000000;
-    scan_delay += (scan_finish.tv_nsec - scan_start.tv_nsec);
-
-    if (scan_delay < LDB_MONITOR_PERIOD * 1000) {
+    clock_get_now(&scan_finish);
+    scan_delay = elapsed_ns(&scan_start, &scan_finish);
+    if (scan_delay < LDB_MONITOR_PERIOD) {
       __time_delay_ns(LDB_MONITOR_PERIOD * 1000 - scan_delay);
     }
 #endif
-  } // while true
-
-  for (int i = 0; i < LDB_MAX_NTHREAD; i++) {
-    free(ldb_tag[i]);
-    free(ldb_ngen[i]);
-    free(ldb_rip[i]);
-    free(ldb_rbp[i]);
-    free(ldb_latency[i]);
   }
-
-  free(ldb_tag);
-  free(ldb_ngen);
-  free(ldb_rip);
-  free(ldb_rbp);
-  free(ldb_latency);
-  free(ldb_cnt);
 
   printf("Monitoring thread exiting...\n");
 
